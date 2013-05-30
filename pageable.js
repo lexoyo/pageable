8511d3f7a4f65d78914fb27a0b7788b51909e641
lexa <a.hoyau@silexlabs.org>
2013-05-30 00:13:37 -0400

    added useDeeplink option

--- pageable.js
+++ pageable.js
@@ -3,29 +3,59 @@
 $.widget('silexlabs.pageable', {
 	version: '1.0.0',
 	options: {
-		defaultPage:"home"
+		defaultPage:"home",
+        useDeeplink:true
 	},
+    currentPage: '',
 	// _setOption is called for each individual option that is changing
 	_setOption: function( key, value ) {
 		this.options[key] = value;
+        switch (key) {
+            case 'useDeeplink':
+                this._destroy();
+                this._create();
+                break;
+        }
 	},
 	_create: function() {
 		console.log("_create ");
-        $(window).bind( 'hashchange', this.updatePage);
+        if(this.useDeeplink){
+            $(window).bind( 'hashchange', this.updatePage);
+        }
+        else{
+            var that=this;
+            this.element.find('a').each(function(){
+                $(this).bind('click', function(event){
+                    event.preventDefault();
+                    console.log($(this).attr('href'));
+                    that.currentPage = $(this).attr('href');
+                    that.updatePage();
+                });
+            });
+        }
         this.updatePage();
 	},
 	_destroy: function() {
 		console.log("_destroy ");
-        $(window).unbind( 'hashchange', this.updatePage);
+        if(this.useDeeplink){
+            $(window).unbind( 'hashchange', this.updatePage);
+        }
+        else{
+            this.element.find('a').each(function(){
+                $(this).unbind('click');
+            });
+        }
 	},
-    updatePage: function (){
-        var newPage = window.location.hash;
-        if (newPage=='') newPage = this.options.defaultPage;
-        else if (newPage.charAt(0)=='#') newPage = newPage.substr(1);
-        console.log(newPage);
+    updatePage: function (event){
+        if(this.useDeeplink){
+            this.currentPage = window.location.hash;
+        }
+        if (this.currentPage=='') this.currentPage = this.options.defaultPage;
+        else if (this.currentPage.charAt(0)=='#') this.currentPage = this.currentPage.substr(1);
+        console.log(this.currentPage);
 
         $('#current-page-style').remove();
-        $('head').append('<style id="current-page-style">.'+newPage+'{display:inherit;}</style>');
+        $('head').append('<style id="current-page-style">.'+this.currentPage+'{display:inherit;}</style>');
     }
 });
 })(jQuery);
